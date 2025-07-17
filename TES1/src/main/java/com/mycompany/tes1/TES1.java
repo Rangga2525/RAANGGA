4f824813ae5b2280f1810194058c993076a6a2d3
Rangga2525 <ranggaimmanuel18@gmail.com>
2025-07-17 10:17:33 +0700

    Add files via upload

---
+++ TES1/src/main/java/com/mycompany/tes1/TES1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tes1;
+
+/**
+ *
+ * @author DELL Latitude
+ */
+public class TES1 {
+    public static void main(String[] args) {
+   
+        String nim="9544500132";
+        String nama="Nasril";
+}
+        biodata Biodata=new Biodata();
+        System.out.println("Nim Mahasiswa:"+Biodata.nim);
+        System.out.println("Nama Mahasiswa:"+Biodata.nama);
+}
